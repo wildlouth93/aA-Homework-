82d90a388484d06e26fc12167c4482d53a9dc7c4
Willie Louth <willielouth@gmail.com>
2019-09-11 21:49:18 -0500

    Added homework for W6D4

---
+++ W6D4/lib/simon.rb
@@ -0,0 +1,73 @@
+class Simon
+  COLORS = %w(red blue green yellow)
+
+  attr_accessor :sequence_length, :game_over, :seq
+
+  def initialize
+    @sequence_length = 1 
+    @game_over = false 
+    @seq = []
+  end
+
+  def play
+    until @game_over
+      take_turn 
+      system("clear")
+    end
+
+    game_over_message
+    reset_game
+  end
+
+  def take_turn
+    show_sequence
+    require_sequence
+
+    unless @game_over 
+      round_success_message 
+      @sequence_length += 1 
+    end
+    
+  end
+
+  def show_sequence
+    add_random_color 
+    @seq.each do |color| 
+      puts color 
+      sleep 0.75 
+      system("clear")
+      sleep 0.25 
+    end
+  end
+
+  def require_sequence
+    puts "Repeat the sequence by entering the first letter of each color on a new line."
+    @seq.each do |color|
+      input = gets.chomp
+      if color[0] != input 
+        @game_over = true 
+        break 
+      end
+    end
+    sleep 0.25 
+  end
+
+  def add_random_color
+    @seq << COLORS.sample 
+  end
+
+  def round_success_message
+    puts "Successful round."
+  end
+
+  def game_over_message
+    puts "Gameover."
+    sleep 1
+  end
+
+  def reset_game
+    @sequence_length = 1 
+    @game_over = false 
+    @seq = []
+  end
+end
